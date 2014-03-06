1f7628d900ed32a0d785dae9ad34ed8ea37d10eb
daviddemello <daviddemello@gmail.com>
2014-03-06 01:12:57 +0100

    adds string replacement to get over stupid behavior of AWS environment variables that treatt he dollar sign as a sepcial character

--- src/ILRProfiles/GenerateBundle/Controller/inc/ilr-faculty-data-conf.php
+++ src/ILRProfiles/GenerateBundle/Controller/inc/ilr-faculty-data-conf.php
@@ -1,7 +1,7 @@
 <?php
 define('AI_API_URL', $this->container->getParameter('ai_api_url'));
 define('AI_USERID', $this->container->getParameter('ai_userid'));
-define('AI_PWD', $this->container->getParameter('ai_pwd'));
+define('AI_PWD', str_replace('Dollar', '$', $this->container->getParameter('ai_pwd')));
 define('LDAP_START', $this->container->getParameter('ldap_start'));
 define('LDAP_FILTER', $this->container->getParameter('ldap_filter'));
 define('LDAP_SERVER', $this->container->getParameter('ldap_server'));
