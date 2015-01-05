b471d88e459e0eb0d500439e58b5da433bc68bc7
David DeMello <daviddemello@gmail.com>
2015-01-05 16:12:27 -0500

    Adds routing to dump certain parameters to verify settings in a particular environment.

--- src/ILRProfiles/GenerateBundle/Controller/GenerateController.php
+++ src/ILRProfiles/GenerateBundle/Controller/GenerateController.php
@@ -19,4 +19,15 @@ class GenerateController extends Controller
         require 'inc/perform_final_xslt_transform.php';
         return $this->render('ILRProfilesGenerateBundle:Generate:index.html.twig', array('job_results' => $job_results));
     }
+
+    public function dumpConstantsAction()
+    {
+        require 'inc/ilr-faculty-data-conf.php';
+        $job_results =
+        'AI_API_URL: ' . AI_API_URL . "\n" .
+        'LDAP_START: ' . LDAP_START . "\n" .
+        'LDAP_FILTER: ' . LDAP_FILTER . "\n" .
+        'ILR_DIRECTORY_LEGACY_DATA_FEED: ' . ILR_DIRECTORY_LEGACY_DATA_FEED . "\n";
+        return $this->render('ILRProfilesGenerateBundle:Generate:index.html.twig', array('job_results' => $job_results));
+    }
 }
