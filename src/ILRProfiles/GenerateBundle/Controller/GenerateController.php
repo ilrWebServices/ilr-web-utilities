2a0b550b1a91c37ef8ef3e6db4bc15594c32f42e
David DeMello <daviddemello@gmail.com>
2015-01-06 01:09:41 -0500

    Refactors to simplify and granularize

--- src/ILRProfiles/GenerateBundle/Controller/GenerateController.php
+++ src/ILRProfiles/GenerateBundle/Controller/GenerateController.php
@@ -8,15 +8,59 @@ class GenerateController extends Controller
 {
     public function indexAction($uid='')
     {
-        require 'inc/ilr-faculty-data-conf.php';
-        require 'inc/get_all_data_use_aws.php';
+        require 'inc/ilr-faculty-data.php';
+
+        add_log_event($job_log, "Job begun");
+        $ldap = write_ldap_xml_to_file($client, $aws_bucket, $job_log);
+        write_legacy_ilr_directory_info_to_file($client, $aws_bucket, $job_log);
+        write_raw_ai_data_to_file($client, $aws_bucket, $ldap, $job_log);
+        write_aggregated_ai_data_to_file($client, $aws_bucket, $job_log);
+        $job_results = log_results($client, $aws_bucket, $job_log, "aggregation and XSL transformation of all ILR faculty and staff profile data");
+
+        return $this->render('ILRProfilesGenerateBundle:Generate:index.html.twig', array('job_results' => $job_results));
+    }
+
+    public function generateLdapAction()
+    {
+        require 'inc/ilr-faculty-data.php';
+
+        add_log_event($job_log, "Job begun");
+        write_ldap_xml_to_file($client, $aws_bucket, $job_log);
+        $job_results = log_results($client, $aws_bucket, $job_log, "collection of ILR LDAP data");
+
+        return $this->render('ILRProfilesGenerateBundle:Generate:index.html.twig', array('job_results' => $job_results));
+    }
+
+    public function generateLegacyAction()
+    {
+        require 'inc/ilr-faculty-data.php';
+
+        add_log_event($job_log, "Job begun");
+        write_legacy_ilr_directory_info_to_file($client, $aws_bucket, $job_log);
+        $job_results = log_results($client, $aws_bucket, $job_log, "collection of legacy website data");
+
+        return $this->render('ILRProfilesGenerateBundle:Generate:index.html.twig', array('job_results' => $job_results));
+    }
+
+    public function generateRawAiAction()
+    {
+        require 'inc/ilr-faculty-data.php';
+
+        add_log_event($job_log, "Job begun");
+        write_raw_ai_data_to_file($client, $aws_bucket, $job_log);
+        $job_results = log_results($client, $aws_bucket, $job_log, "collection of raw Activity Insight");
+
         return $this->render('ILRProfilesGenerateBundle:Generate:index.html.twig', array('job_results' => $job_results));
     }
 
     public function retransformAction()
     {
-        require 'inc/ilr-faculty-data-conf.php';
-        require 'inc/perform_final_xslt_transform.php';
+        require 'inc/ilr-faculty-data.php';
+
+        add_log_event($job_log, "Job begun");
+        write_aggregated_ai_data_to_file($client, $aws_bucket, $job_log);
+        $job_results = log_results($client, $aws_bucket, $job_log, "XSL re-transformation only of exisitng profile data for all ILR faculty and staff");
+
         return $this->render('ILRProfilesGenerateBundle:Generate:index.html.twig', array('job_results' => $job_results));
     }
 
--- src/ILRProfiles/GenerateBundle/Controller/inc/ilr-faculty-data.php
+++ src/ILRProfiles/GenerateBundle/Controller/inc/ilr-faculty-data.php
@@ -7,7 +7,23 @@
  *
  */
 
-// require 'ilr-faculty-data-conf.php';
+require 'ilr-faculty-data-conf.php';
+
+date_default_timezone_set('EST');
+
+$aws_key = AWS_KEY;
+$aws_secret = AWS_SECRET;
+$aws_bucket = AWS_BUCKET;
+
+use Aws\S3\S3Client;
+$client = S3Client::factory(array(
+  'key' => $aws_key,
+  'secret' => $aws_secret,
+));
+
+// Register the stream wrapper from an S3Client object
+$client->registerStreamWrapper();
+$job_log = array();
 
 function verify_configuration() {
   $result = true;
@@ -351,4 +367,96 @@ function display_log($log) {
   $result .= "\nTotal execution time: {$total_time} seconds.\n";
   return $result;
 }
+
+// Write log results to file and also return as string
+function log_results(&$aws_Client, $aws_bucket, &$job_log, $job_title) {
+  $ip_tracking = !empty($_SERVER['REMOTE_ADDR']) ? "(requested from IP: {$_SERVER['REMOTE_ADDR']})" : '(from local CLI script execution)';
+  $job_results = "Results of {$job_title} {$ip_tracking}:\n" . display_log($job_log);
+  $log_file_name = 'feed-generator-report-' . date('Y-n-j-H-i-s', time()) . '.txt';
+  file_put_contents("s3://{$aws_bucket}/{$log_file_name}", $job_results);
+  set_perms($aws_Client, $aws_bucket, $log_file_name);
+  return $job_results;
+}
+
+// Makes a file in an S3 bucket publicly readable.
+function set_perms(&$aws_Client, $bucket, $file_name) {
+  $aws_Client->putObjectAcl(array(
+    'Bucket'     => $bucket,
+    'Key'        => $file_name,
+    'ACL'        => 'public-read'
+  ));
+}
+
+// Deletes a file in an S3 bucket.
+function delete_file($aws_bucket, $file_name) {
+  if (file_exists("s3://{$aws_bucket}/{$file_name}")) {
+    unlink("s3://{$aws_bucket}/{$file_name}");
+  }
+}
+
+// Repalces a file in an S3 bucket with a new version.
+function replace_file(&$aws_Client, $aws_bucket, $file_name, $output) {
+  delete_file($aws_bucket, $file_name);
+  file_put_contents("s3://{$aws_bucket}/{$file_name}", $output);
+  set_perms($aws_Client, $aws_bucket, $file_name);
+}
+
+// Functions to write data files
+
+// LDAP to file
+function write_ldap_xml_to_file(&$aws_Client, $aws_bucket, &$job_log) {
+  $ldap = get_ilr_people_from_ldap();
+  replace_file($aws_Client, $aws_bucket, 'ldap.xml', ldap2xml($ldap));
+  add_log_event($job_log, "LDAP file created");
+  return $ldap;
+}
+
+// Legacy ILR directory data to file
+function write_legacy_ilr_directory_info_to_file(&$aws_Client, $aws_bucket, &$job_log) {
+  $ilrweb_data = get_legacy_ilr_directory_info();
+  replace_file($aws_Client, $aws_bucket, 'legacy_ilr_directory_HTML.xml', $ilrweb_data);
+  add_log_event($job_log, "Legacy ILR Profile data collected");
+}
+
+// Raw AI data to file
+function write_raw_ai_data_to_file(&$aws_Client, $aws_bucket, $ldap, &$job_log) {
+  delete_file($aws_bucket, "ilr_profiles_raw_ai_data.xml");
+  $stream = fopen("s3://{$aws_bucket}/ilr_profiles_raw_ai_data.xml", 'w');
+
+  fwrite($stream, '<?xml version="1.0" encoding="UTF-8"?>
+  <Data xmlns="http://www.digitalmeasures.com/schema/data" xmlns:dmd="http://www.digitalmeasures.com/schema/data-metadata" dmd:date="2014-01-14">');
+  $count = 0;
+  // For each person returned by the ldap query, Append appropriate xml to xml/ilr_people.xml
+  foreach( $ldap as $person) {
+    $count += 1;
+    if ($person['uid'][0] != '') {
+      //   Try to get person info from Activity Insights
+      $ai_data = get_ai_person($person['uid'][0]);
+
+      if ( $ai_data->statusCode == 200 ) {  // Activity Insight returned data for this person
+        // Add Activity Insight data to the main XML document
+        fwrite($stream, get_ai_record_from_data($ai_data->responseData));
+      } else {
+        // Add a placeholder Record to the main XML document with the userid
+        fwrite($stream, '<Record username="' . $person['uid'][0] . '" />');
+      }
+    }
+  }
+
+  fwrite($stream, '<recordcount>' . $count . '</recordcount></Data>');
+  fclose($stream);
+  set_perms($aws_Client, $aws_bucket, 'ilr_profiles_raw_ai_data.xml');
+  add_log_event($job_log, "Raw Activity Insight data collected");
+}
+
+// Aggregated and transformed data to file
+function write_aggregated_ai_data_to_file(&$aws_Client, $aws_bucket, &$job_log) {
+  // Retrieve to XML
+  $raw_xml = file_get_contents("s3://{$aws_bucket}/ilr_profiles_raw_ai_data.xml");
+
+  // Run the XSLT transform on the main xml file, which will fold in the fields from lpad and legacy_ilr_directory_HTML
+  replace_file($aws_Client, $aws_bucket, 'ilr_profiles_feed.xml', stripEmptyCDATA(xslt_transform($raw_xml, get_ilr_profiles_transform_xsl(), 'xml')));
+  add_log_event($job_log, "Final ILR Profiles data feed generated");
+}
+
 
