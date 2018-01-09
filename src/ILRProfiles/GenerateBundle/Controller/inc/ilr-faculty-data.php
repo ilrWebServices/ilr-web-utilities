da1a7fd099bb3d6d41b5ee11c1b0ef373418e21b
David DeMello <daviddemello@gmail.com>
2018-01-09 12:35:38 -0500

    Adds new people to the list of faculty for whom to display full profiles.

--- src/ILRProfiles/GenerateBundle/Controller/inc/ilr-faculty-data.php
+++ src/ILRProfiles/GenerateBundle/Controller/inc/ilr-faculty-data.php
@@ -291,7 +291,7 @@ function ldap2xml($ldap) {
     $faculty_titles[] = 'Research Associate';
     $faculty_titles[] = 'Scholar Visit';
 
-    $temp_faculty = array('lha1', 'gc32', 'ljf8', 'lsg3', 'vmb2', 'zen2', 'srt82', 'so44', 'jds13','lrs95', 'elg234', 'jn497', 'lhc62');
+    $temp_faculty = array('lha1', 'gc32', 'ljf8', 'lsg3', 'vmb2', 'zen2', 'srt82', 'so44', 'jds13','lrs95', 'elg234', 'jn497', 'lhc62', 'mjb62', 'cjb39', 'nah36', 'ak839', 'kal238', 'ko259', 'mcs378', 'ws283', 'jt693');
     $deans = array('kfh7', 'ljb239', 'jeg68', 'rss14');
     $faculty_leave = get_faculty_leave();
 
